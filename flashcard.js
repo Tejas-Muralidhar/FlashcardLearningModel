3cfe7215ff0367b9b344259d747b4f6f31719039
Tejas <1rn21cs171.Tejas@gmail.com>
2023-07-26 23:44:35 +0530

    added html, css and js. Mainly added animation

--- flashcard.js
+++ flashcard.js
@@ -1,4 +1,12 @@
-function SayHello(){
-    const button = document.querySelector('#button')
-    button.previousElementSibling.innerText = "Say Hello!";
-}    gudugudugudigudugugdu
+let toggle_button = document.querySelectorAll(".card");
+toggle_button.forEach(element => {
+    element.addEventListener("click",(e)=>{
+        if(element.style.backgroundColor === "black"){
+            element.style.backgroundColor = "whitesmoke";
+        }
+        else{
+            element.style.backgroundColor = "black";
+        }    
+    });
+});
+//This helps toggle between questions and answers
