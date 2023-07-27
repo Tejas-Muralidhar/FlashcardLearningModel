82550db2053416e745e18bb45450806cfe41df36
Tejas <1rn21cs171.Tejas@gmail.com>
2023-07-27 20:07:47 +0530

    idk why im uploading sanjana will anyway not see this

--- flashcard.js
+++ flashcard.js
@@ -1,4 +1,3 @@
-// *********************************************************
 function revealAnswer(event) {
     var card = event.currentTarget;
     var answer = card.querySelector('.answer');
@@ -30,36 +29,3 @@ function addEventListenersToCards() {
 // Call the function to add event listeners to all card divs within the Deck
 addEventListenersToCards();
 
-
-
-// Function to get query parameters from the URL
-function getQueryParam(param) {
-    const query = window.location.search;
-    const parts = query.split('=');
-      if (parts.length === 2) {
-        return parts[1];
-      }
-  }
-
-  // Function to filter and display relevant flashcards based on the topic
-  function showRelevantFlashcards(topic) {
-    // Get all flashcard divs
-    const decks = document.querySelectorAll('.Deck');
-
-    // Loop through each flashcard div and hide/show based on the topic
-    decks.forEach((deck) => {
-      if (deck.id === topic) {
-        deck.style.display = 'flex'; // Show the flashcard
-      } else {
-        deck.style.display = 'none';  // Hide the flashcard
-      }
-    });
-  }
-
-  // Get the selected topic from the query parameter
-  const selectedTopic = getQueryParam('topic');
-
-  // If a topic is selected, show relevant flashcards
-  if (selectedTopic!=null) {
-    showRelevantFlashcards(selectedTopic);
-  }
